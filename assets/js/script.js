c0b4198c62b1a7d5e182d953d850d1a5b6f76d48
Georgia Hoost <gfroehling@icloud.com>
2021-02-08 21:02:26 +0100

    fxxxing button colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,33 @@
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
 const pScore = document.getElementById('human');
 const cScore = document.getElementById('computer');
-const round5 = document.getElementById('.five');
-const round10 = document.getElementById('.ten');
-const round15 = document.getElementById('.fifteen');
-const round20 = document.getElementById('.twenty');
+let letsPlay = document.getElementById('letsPlay');
 
 const buttons = document.querySelectorAll('#symbols button');
 let variations = ['rock', 'paper', 'scissor']
 
 let restart = document.getElementById('restart');
 
-let home = document.getElementById('human').innerHTML = playerScore;
-let comp = document.getElementById('computer').innerHTML = computerScore;
-
-// // round5.onclick = round5.value.innerHTML = "5";
-// <input onclick=“getRounds(10)” type=“radio” name=“value” id=“value10" value=“10”>
 
 
-
-
-function getRounds(i)
+function showPoints(home, computer)
 {
-    round5.onclick = getRounds(5);
-    round10.onclick = getRounds(10);
-    round15.onclick = getRounds(15);
-    round20.onclick = getRounds(20);
-    console.log(i);
-    getRounds(i);
+    document.getElementById('human').innerHTML = home;
+    document.getElementById('computer').innerHTML = computer;
 };
 
 
+restart.addEventListener('click', e => 
+{
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    showPoints(0, 0);
+
+});
 
 
 buttons.forEach((button) =>
@@ -43,21 +38,37 @@ buttons.forEach((button) =>
         let computerChoice = variations[Math.floor(Math.random() * (variations.length))];
         console.log(computerChoice);
 
+        if (document.querySelector('input[name="rounds"]:checked').value == null) {
+            document.getElementById('letsPlay').innerHTML = "let's play!";
+        };
+
+
         if (computerChoice === 'rock' && button.className === 'paper') {
-            home++;
+            playerScore++;
         } else if (computerChoice === 'rock' && button.className === 'scissor') {
-            comp++;
+            computerScore++;
         } else if (computerChoice === 'paper' && button.className === 'rock') {
-            comp++;
+            computerScore++;
         } else if (computerChoice === 'paper' && button.className === 'scissor') {
-            home++;
+            playerScore++;
         } else if (computerChoice === 'scissor' && button.className === 'rock') {
-            home++;
+            playerScore++;
         } else if (computerChoice === 'scissor' && button.className === 'paper') {
-            comp++;
+            computerScore++;
         } else if (computerChoice === button.className) {
-            document.getElementsByClassName('.dots').innerHTML = "It's a tie!";
+            computerScore + 0;
+            playerScore + 0;
         }
+        round++;
+        showPoints(playerScore, computerScore);
+
+
+        if (document.querySelector('input[name="rounds"]:checked').value == round) {
+            document.getElementById('letsPlay').innerHTML = 'GAME OVER';
+        };
+
+
     });
 
 });
+showPoints(playerScore, computerScore);
